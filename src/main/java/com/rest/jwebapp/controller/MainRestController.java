5c2bb4373280e5e1108a6772b9ed0e8eff9b14c2
Igor Wysocki <lgorvysocky@gmail.com>
2020-06-23 22:25:51 +0200

    init (new)

--- src/main/java/com/rest/jwebapp/controller/MainRestController.java
+++ src/main/java/com/rest/jwebapp/controller/MainRestController.java
@@ -1,14 +1,15 @@
 package com.rest.jwebapp.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.rest.jwebapp.exception.NotFoundException;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.*;
 
 @RestController
 @RequestMapping("collection")
 public class MainRestController {
+    private int counter = 4;
+
     public List<Map<String, String>> collection_data = new ArrayList<Map<String, String>>() {
         {
             add(new HashMap<String, String>() {
@@ -39,7 +40,47 @@ public class MainRestController {
     };
 
     @GetMapping
-    public List<Map<String, String>> listData() {
+    public List<Map<String, String>> list() {
         return this.collection_data;
+    }
+
+    @GetMapping("{id}")
+    public Map<String, String> get(
+            @PathVariable String id
+    ) {
+        return this.getMessage(id);
+    }
+
+    @PostMapping
+    public Map<String, String> create(
+            @RequestBody Map<String,
+                    String> message) {
+        message.put("id", String.valueOf(counter++));
+        this.collection_data.add(message);
+        return message;
+    }
+
+    @PutMapping("{id}")
+    public Map<String, String> update(
+            @PathVariable String id,
+            @RequestBody Map<String, String> message) {
+        Map<String, String> collection_data_fromDB = getMessage(id);
+        collection_data_fromDB.putAll(message);
+        collection_data_fromDB.put("id", id);
+        return collection_data_fromDB;
+    }
+
+    @DeleteMapping("{id}")
+    public void delete(@PathVariable String id) {
+        Map<String, String> message = getMessage(id);
+        this.collection_data.remove(message);
+    }
+
+    private Map<String, String> getMessage(@PathVariable String id) {
+        return this.collection_data
+                .stream()
+                .filter(message -> message.get("id").equals(id))
+                .findFirst()
+                .orElseThrow(NotFoundException::new);
     }
 }
