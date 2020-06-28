b20357ff108881bbdba09fd5975bbd6f9852f562
Igor Wysocki <lgorvysocky@gmail.com>
2020-06-28 23:07:12 +0200

    added vuetify

--- src/main/resources/static/js/components/messages/MessageForm.vue
+++ src/main/resources/static/js/components/messages/MessageForm.vue
@@ -1,7 +1,15 @@
 <template>
     <div>
-        <input type="text" placeholder="Write something" v-model="text" />
-        <input type="button" value="Save" @click="save" />
+        <v-layout row>
+            <v-text-field
+                    label="New message"
+                    placeholder="Write something"
+                    v-model="text"
+            />
+            <v-btn @click="save">
+                Save
+            </v-btn>
+        </v-layout>
     </div>
 </template>
 
